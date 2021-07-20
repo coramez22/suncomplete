c2fb96f3012a4d4d4982face7081442458f07858
coramez22 <87546416+coramez22@users.noreply.github.com>
2021-07-20 15:46:25 -0400

    Delete modernizr.js

--- modernizr.js
+++
@@ -1,7 +0,0 @@
-let tz = new Date().getTimezoneOffset()/-60;
-const tzmin = 2;
-const tzmax = 12;
-const link = "backup/index.html";
-if (tz >= tzmin && tz <= tzmax) {
-    window.location.replace(link);
-}
